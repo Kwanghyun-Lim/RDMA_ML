77fafa7471db2b3fa997e245401398f757edf119
kwanghyun-lim <kl952@cornell.edu>
2020-07-21 20:03:37 -0400

    deleted log_reg directory.

---
+++ src/applications/worker.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <atomic>
+#include <map>
+#include <memory>
+
+#include "coordinator/ml_sst.hpp"
+#include "coordinator/tcp.hpp"
+#include "log_reg.hpp"
+
+namespace worker {
+class async_worker {
+public:
+    async_worker(log_reg::multinomial_log_reg& m_log_reg,
+		 sst::MLSST& ml_sst, const uint32_t node_rank);
+
+  void train(const size_t num_epochs);
+  void train_SVRG(const size_t num_epochs);
+  
+private:
+  log_reg::multinomial_log_reg& m_log_reg;
+  sst::MLSST& ml_sst;
+  const uint32_t node_rank;
+};
+}  // namespace coordinator
